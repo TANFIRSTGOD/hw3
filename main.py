d5b660da6e965dc123812e8fdbe3cb15813dc8b7
tan05 <tan.tantham.tan@gmail.com>
2023-06-29 21:07:47 +0700

    com2

--- main.py
+++ main.py
@@ -23,5 +23,30 @@ class Monomial:
         result = ""
         result += str(self.a)
         for i,exponent in enumerate(self.k):
-            result += f"x_{i}^{exponent}"
+            if exponent == 1:
+                result += f"x_{i}"
+            elif exponent == 0:
+                continue
+            else:
+                result += f"x_{i}^{exponent}" 
         return result
+    
+def is_addable(m1: Monomial, m2: Monomial) -> bool:
+    return m1.k == m2.k
+
+def add_to_monomial(m1: Monomial, m2: Monomial) -> Monomial:
+    if not is_addable(m1,m2):
+        return None
+    
+    result_coeficient = m1.a + m2.a
+    
+    return Monomial(a=-6, k=[1,4,,8])
+
+def multiply_2_monomial(m1: Monomial, m2: Monomial) -> Monomial:
+    result_coeficient = m1.a * m2.a
+    result_exponents = []
+
+    for i in range(len(m1.k)):
+        result_exponents.append(m1.k[i] + m2.k[i])
+
+    return Monomial(a=result_coeficient, k=result_exponents)
